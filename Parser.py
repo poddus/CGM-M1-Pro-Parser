1353e10157e93156d13155aaf7d4b3b0605ec6a9
Leopold von Seckendorff <leopold@seckendorff.com>
2021-07-17 00:12:01 +0200

    add entry classes, complete parsing of 1. line in GO-Fehler

--- Parser.py
+++ Parser.py
@@ -9,17 +9,71 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
+class CGMPatient:
+    """
+    base class which defines a generic single entry
+    """
+    def __init__(self, patient_buffer):
+        self.pat_id = patient_buffer['pat_id']
+        self.first_name = patient_buffer['first_name']
+        self.last_name = patient_buffer['last_name']
+        self.birth_date = patient_buffer['birth_date']
+
+    def fullname(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+
+class CGMBillingNotice(CGMPatient):
+    """
+    represents single entry in GO-Fehler
+
+    quarter is of the format
+    [1,2,3,4][yyyy] (for example 12000 for 1. quarter of year 2000)
+
+    insurance status can be
+    M (Mitglied)
+    F (Familie)
+    R (Rentner)
+
+    VKNR (Vertragskassennummer)
+
+    KTAB (Kostenträgerabrechnungsbereich)
+    00 = Primärabrechnung
+    01 = Sozialversicherungsabkommen (SVA)
+    02 = Bundesversorgungsgesetz (BVG)
+    03 = Bundesentschädigungsgesetz (BEG)
+    07 = Bundesvertriebenengesetz (BVFG)
+    """
+    def __init__(self, patient_buffer, insurance_buffer, notices=None):
+        super().__init__(patient_buffer)
+
+        self.treatment_type = insurance_buffer['treatment_type']
+        self.quarter = insurance_buffer['quarter']
+        self.ins_status = insurance_buffer['ins_status']
+        self.vknr = insurance_buffer['vknr']
+        self.ktab = insurance_buffer['ktab']
+
+        if notices:
+            self.notices = notices
+        else:
+            self.notices = []
+
+
+class CGMPatientRecord(CGMPatient):
+    """represents single entry in Textgruppenstatistik"""
+    def __init__(self, patient_buffer, chart_notes=None):
+        super().__init__(patient_buffer)
+        if chart_notes:
+            self.chart_notes = chart_notes
+        else:
+            self.chart_notes = []
+
+
 class CGMParser:
     """
-    keep track of position and state, format input.
+    Parser for lists exported from CGM M1 Pro
 
-    possible states:
-    header (default)
-    patient
-    notice
-    footer
-
-    possible input types:
+    supported input types:
     GO-Fehler (1. von 3 Protokollen bei der Kassenabrechnung)
     TGS (Textgruppenstatistik)
     """
@@ -38,20 +92,14 @@ class CGMParser:
     # input types
     T_GOF = 'GO-Fehler'
     T_TGS = 'TGS'
-    # states
-    S_HEADER = 'HEADER'
-    S_PATIENT = 'PATIENT'
-    S_NOTICE = 'NOTICE'
-    S_FOOTER = 'FOOTER'
     # regex searches
     PAT_DEL = r'={85} {6}\n'
     TGS_ENTRY_DEL = r'-{96}'
-    PAT_NAME = r'[\w ÄäÖöÜüß-]*'
-    DATE = r'\d{2}.\d{2}.\d{4}'
+    # PAT_NAME = r'[\w ÄäÖöÜüß-]*'
+    # DATE = r'\d{2}.\d{2}.\d{4}'
 
     def __init__(self):
         self.input_type = ''
-        self.state = self.S_HEADER
         self.rel_pos = 0
 
         self.entries = []
@@ -67,8 +115,6 @@ class CGMParser:
         elif h == self.TGS_HEADER:
             self.input_type = self.T_TGS
         logging.info('input type set to: {}'.format(self.input_type))
-        logging.info('set state to: {}'.format(self.S_PATIENT))
-        self.state = self.S_PATIENT
         return data
 
     def parse_date(self, y, m, d):
@@ -76,14 +122,67 @@ class CGMParser:
 
     def separate_entries(self, data):
         if self.input_type == self.T_GOF:
-            logging.info('sep data using GOF format')
-            data_str = ''.join(data)
-            entries = re.split(self.PAT_DEL, data_str)
-            trimmed = []
+            logging.info('separating data using GOF format')
+
+            # remove first entry delimiter
+            del data[0]
+
+            entries = []
+            entry_buffer = []
+            for line in data:
+                trimmed_line = ''
+                if re.match(self.PAT_DEL, line):
+                    logging.info('patient delimiter encountered')
+                    entries.append(entry_buffer)
+                    entry_buffer = []
+                    continue
+                trimmed_line = re.sub('^ *| *\n *| *$', '', line)
+                entry_buffer.append(trimmed_line)
+
+            return entries
+
+    def convert_to_datetime(y, m, d):
+        return date(
+            int(y),
+            int(m),
+            int(d)
+        )
+
+    def parse_notices(self, entries):
+        pass
+
+    def parse_entries(self, entries):
+        entries_new = []
+        if self.input_type == self.T_GOF:
+            logging.info('parsing entries using GOF format')
+
             for e in entries:
-                trimmed.append(re.sub(' *\n *| *$', '\n', e))
-            del trimmed[0]
-            return trimmed
+                # parse first line
+                patient_buffer = {}
+                match_pat = re.search(
+                    '(^\d*)\s([\w ÄäÖöÜüß-]*), ([\w ÄäÖöÜüß-]*)\s(\d{2}).(\d{2}).(\d{4})',
+                    e[0]
+                )
+                if match_pat:
+                    pat_birth_date = self.convert_to_datetime(
+                        int(match_pat[6]),
+                        int(match_pat[5]),
+                        int(match_pat[4])
+                    )
+                    patient_buffer['pat_id'] = match_pat[1]
+                    patient_buffer['last_name'] = match_pat[2]
+                    patient_buffer['first_name'] = match_pat[3]
+                    patient_buffer['birth_date'] = pat_birth_date
+
+                # parse second line
+                # TODO
+                insurance_buffer = {}
+
+                # parse notices
+                # TODO
+                notices = self.parse_notices(entries[2:])
+
+                entries_new.append(CGMBillingNotice(patient_buffer, insurance_buffer, notices))
 
 
 p = CGMParser()
